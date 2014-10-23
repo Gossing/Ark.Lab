5f58496fbb8f066481e161b53799dd23ba7e55ef
Seungyoun Ko <knormaluser@gmail.com>
2014-10-24 08:35:04 +0900

    Update CreateTable.sql
    wmp_moneyio 테이블 SQL 수정

--- 01.WeeklyMoneyPlan/CreateTable.sql
+++ 01.WeeklyMoneyPlan/CreateTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `knormaluser`.`wmp_user` (
-  `user_id`   VARCHAR( 16 ) NOT NULL COMMENT '사용자 ID',
-  `user_pswd` VARCHAR( 64 ) NOT NULL COMMENT '사용자 PSWD',
+  `user_id`   VARCHAR( 16 ) NOT NULL COMMENT '사용자ID',
+  `user_pswd` VARCHAR( 64 ) NOT NULL COMMENT '사용자PASS',
   `nick_nm`   VARCHAR( 20 ) NOT NULL COMMENT '별명',
   `email`     VARCHAR( 32 ) NOT NULL COMMENT '이메일',
 UNIQUE ( `user_id` ,`email` )
@@ -10,13 +10,13 @@ COMMENT = '사용자 정보';
 
 
 CREATE TABLE `knormaluser`.`wmp_moneyio` (
-  `user_id`   VARCHAR( 16 )   NOT NULL COMMENT '사용자  ID',
+  `user_id`   VARCHAR( 16 )   NOT NULL COMMENT '사용자ID',
   `proc_date` DATE            NOT NULL COMMENT '기준일자',
-  `io_type`   VARCHAR( 1 )    NOT NULL COMMENT '처리구분',
-  `comment`   VARCHAR( 128 )  NOT NULL COMMENT '내용',
-  `amount`    DOUBLE( 16 )    NOT NULL COMMENT '금액',
-  `card_type` VARCHAR( 1 )    NOT NULL COMMENT 'CARD 구분',
-  `card_id`   VARCHAR( 6 )    NOT NULL COMMENT 'CARD ID',
+  `io_tp`     VARCHAR( 1 )    NOT NULL COMMENT '처리구분',
+  `memo`      VARCHAR( 128 )  NOT NULL COMMENT '내용',
+  `amount`    DOUBLE          NOT NULL COMMENT '금액',
+  `card_tp`   VARCHAR( 1 )    NOT NULL COMMENT '카드구분',
+  `card_id`   VARCHAR( 6 )    NOT NULL COMMENT '카드ID',
 UNIQUE ( `user_id`  )
 )
 ENGINE = InnoDB
